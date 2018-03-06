f0fa1d78b305d586ada2ef1d8683aca7208922fb
carlos <cp614@cam.ac.uk>
2018-03-06 12:26:44 +0000

    Initial Commit

---
+++ AdaptiveKiosk/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AdaptiveKiosk
+//
+//  Created by Carlos Purves on 06/03/2018.
+//  Copyright © 2018 Carlos Purves. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ AdaptiveKiosk/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AdaptiveKiosk
+//
+//  Created by Carlos Purves on 06/03/2018.
+//  Copyright © 2018 Carlos Purves. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
